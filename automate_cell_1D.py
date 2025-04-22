2f25032912ce9767d0ceb6df3efcda7194948477
Joué Axel <113608406+Bresingre@users.noreply.github.com>
2025-04-22 18:10:22 +0200

    ajout affichage avec matplotlib

--- automate_cell_1D.py
+++ automate_cell_1D.py
@@ -1,12 +1,17 @@
 import time
-
+import matplotlib.pyplot as plt
 
 ligne_cell = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+#tableau pour l'affichage
+affich_plt = []
 
-for g in range(10):
+for g in range(50):
     ligne_remplace = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
     print (ligne_cell)
-    time.sleep(2)  #cmb de temps entre générations
+    time.sleep(0.1)  #cmb de temps entre générations
+
+    affich_plt.append(ligne_cell.copy())
+    
     for i in range(1,len(ligne_cell)-1): #pas touche aux bords
 
         if ligne_cell[i-1] == 0 and ligne_cell[i] == 0 and ligne_cell[i+1] == 0:
@@ -35,3 +40,7 @@ for g in range(10):
 
     ligne_cell = ligne_remplace
 
+plt.imshow(affich_plt, cmap="plasma", interpolation="nearest")
+plt.title("Automate Cellulaire 1D suivant une rêgle 30")
+plt.axis("off")
+plt.show()
